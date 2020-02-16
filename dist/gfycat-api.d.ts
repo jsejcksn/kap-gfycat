706de3b60fbbde3e9ce37e4aba0b70a69fc5c18c
Jesse Jackson <jsejcksn@users.noreply.github.com>
2020-02-16 12:40:32 -0600

    Build

--- dist/gfycat-api.d.ts
+++ dist/gfycat-api.d.ts
@@ -1,76 +1,34 @@
 /// <reference types="node" />
 import * as fs from 'fs';
-declare class UploadResponses {
-    all: this['complete'] | this['duplicate'] | this['encoding'] | this['error'] | this['notFound'];
-    complete: {
-        gfyname: string;
-        task: 'complete';
-    };
-    duplicate: {
-        avgColor?: any;
-        createDate?: any;
-        description?: any;
-        dislikes?: any;
-        extraLemmas?: any;
-        frameRate?: any;
-        gfyId?: any;
-        gfyname?: any;
-        gfyName?: any;
-        gfyNumber?: any;
-        gifSize?: any;
-        gifUrl?: any;
-        height?: any;
-        languageCategories?: any;
-        likes?: any;
-        max2mbGif?: any;
-        max5mbGif?: any;
-        md5?: any;
-        md5Found: 1;
-        mobilePosterUrl?: any;
-        mobileUrl?: any;
-        mp4Size?: any;
-        mp4Url?: any;
-        nsfw?: any;
-        numFrames?: any;
-        posterUrl?: any;
-        published?: any;
-        source?: any;
-        tags?: any;
-        task?: any;
-        thumb100PosterUrl?: any;
-        title?: any;
-        userName?: any;
-        views?: any;
-        webmSize?: any;
-        webmUrl?: any;
-        width?: any;
-    };
-    encoding: {
-        task: 'encoding';
-        time: number;
-    };
-    error: {
-        errorMessage: {
-            code: string;
-            description: string;
-        };
-        task: 'error';
-    };
-    notFound: {
-        task: 'NotFoundo';
-        time: number;
-    };
-    unique: this['complete'] | this['encoding'] | this['error'] | this['notFound'];
+interface UploadResponseComplete {
+    gfyname: string;
+    task: 'complete';
 }
+interface UploadResponseEncoding {
+    task: 'encoding';
+    time: number;
+}
+interface UploadResponseError {
+    errorMessage: {
+        code: string;
+        description: string;
+    };
+    task: 'error';
+}
+interface UploadResponseNotFound {
+    task: 'NotFoundo';
+    time: number;
+}
+declare type UploadResponseUnique = UploadResponseComplete | UploadResponseEncoding | UploadResponseError | UploadResponseNotFound;
 declare class GfycatApi {
     accessToken: string;
     clientId: string;
     clientSecret: string;
     gfyname: string;
     constructor(clientId: string, clientSecret: string);
-    authenticate(): Promise<string | never>;
-    checkUploadStatus(): Promise<UploadResponses['unique'] | never>;
-    getUploadId(): Promise<string | never>;
-    uploadFile(filePath: fs.PathLike): Promise<void | never>;
+    authenticate(): Promise<string>;
+    checkUploadStatus(): Promise<UploadResponseUnique>;
+    getUploadId(): Promise<string>;
+    uploadFile(filePath: fs.PathLike): Promise<void>;
 }
 export { GfycatApi, GfycatApi as default };
